0e4757a9bddf5a969d45a001b24c39ff216274ee
Angel Gutierrez <50065370+agutierrez0@users.noreply.github.com>
2021-11-28 19:22:18 -0500

    Update BLE_server_working.ino

--- BLE_server_working.ino
+++ BLE_server_working.ino
@@ -1,26 +1,32 @@
-/*
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updates by chegewara
-*/
-
+#include <HCSR04.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+UltraSonicDistanceSensor ultrasonic(2,4);
+int distance;
+
+// left motor
+int leftMotorSpeedPin = 14;
+int leftMotorForwardPin = 27;
+int leftMotorBackwardPin = 26;
+
+
+// right motor
+int rightMotorSpeedPin = 21;
+int rightMotorForwardPin = 18;
+int rightMotorBackwardPin = 19;
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define OTHER_CHARACTERISTIC_UUID "5d0faa6a-5086-11ec-bf63-0242ac130002"
 
 int count = 0;
 bool device_connected = false;
 BLEServer *pServer;
 BLEService *pService;
 BLECharacteristic *pCharacteristic;
-
-static BLERemoteCharacteristic* pRemoteCharacteristic;
+BLECharacteristic *otherCharacteristic;
 
 class MyServerCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
@@ -32,7 +38,18 @@ class MyServerCallbacks: public BLEServerCallbacks {
   };
 };
 void setup() {
-  Serial.begin(115200);
+    pinMode(leftMotorSpeedPin, OUTPUT);
+    pinMode(leftMotorForwardPin, OUTPUT);
+    pinMode(leftMotorBackwardPin, OUTPUT);
+    pinMode(rightMotorSpeedPin, OUTPUT);
+    pinMode(rightMotorForwardPin, OUTPUT);
+    pinMode(rightMotorBackwardPin, OUTPUT);
+
+    Serial.begin(9600);
+
+  digitalWrite(leftMotorSpeedPin, HIGH);
+  digitalWrite(rightMotorSpeedPin, HIGH);
+  
   Serial.println("Starting BLE work!");
 
   BLEDevice::init("Long name works now");
@@ -45,7 +62,16 @@ void setup() {
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
-  pCharacteristic->setValue("Hello World says Neil");
+  pCharacteristic->setValue("Hello World says Mani");
+
+  otherCharacteristic = pService->createCharacteristic(
+                                         OTHER_CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  otherCharacteristic->setValue("Hello World says Angel");
+  
   pService->start();
   // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
@@ -58,20 +84,87 @@ void setup() {
 }
 
 void loop() {
+  distance = ultrasonic.measureDistanceCm(); //Use 'CM' for centimeters or 'INC' for inches
+  Serial.println(distance);
+  otherCharacteristic->setValue(distance);
+  otherCharacteristic->notify();
+
+  if (distance > -1 && distance < 15) {
+    stop();
+    pCharacteristic->setValue("stopping");
+    delay(1000);
+    goBackward();
+    pCharacteristic->setValue("moving backwards");
+    delay(300);
+    stop();
+    delay(1000);
+    
+    if(random(0, 2) == 0) {
+      goLeft();
+      pCharacteristic->setValue("moving left");
+    } else {
+      goRight();
+      pCharacteristic->setValue("moving right");
+    }
+    
+    delay(500);
+    stop();
+    pCharacteristic->setValue("stopping");
+    delay(700);
+  } else {
+    goForward();
+    pCharacteristic->setValue("moving forward");
+  }
+   
+  pCharacteristic->notify();
   if (device_connected) {
     Serial.println("Device connected!");
   } else {
     Serial.println("Device not connected!");
   }
-
-  if (true) {
-    Serial.println("Can read!");
-  } else {
-    Serial.println("Can't read!");
-  }
+  
   Serial.println("Test");
-  pCharacteristic->setValue(count);
-  pCharacteristic->notify();
+ 
   count = count + 1;
   delay(3000);
+}
+
+void goForward() {
+  Serial.println("MOVING FORWARD");
+  digitalWrite(leftMotorForwardPin, HIGH);
+  digitalWrite(leftMotorBackwardPin, LOW);
+  digitalWrite(rightMotorForwardPin, HIGH);
+  digitalWrite(rightMotorBackwardPin, LOW);
+}
+
+void goBackward() {
+  Serial.println("MOVING BACKWARDS");
+  digitalWrite(leftMotorForwardPin, LOW);
+  digitalWrite(leftMotorBackwardPin, HIGH);
+  digitalWrite(rightMotorForwardPin, LOW);
+  digitalWrite(rightMotorBackwardPin, HIGH);
+}
+
+void stop() {
+  Serial.println("STOP");
+  digitalWrite(leftMotorForwardPin, LOW);
+  digitalWrite(leftMotorBackwardPin, LOW);
+  digitalWrite(rightMotorForwardPin, LOW);
+  digitalWrite(rightMotorBackwardPin, LOW);
+}
+
+void goRight() {
+  Serial.println("RIGHT");
+  digitalWrite(leftMotorForwardPin, HIGH);
+  digitalWrite(leftMotorBackwardPin, LOW);
+  digitalWrite(rightMotorForwardPin, LOW);
+  digitalWrite(rightMotorBackwardPin, HIGH);
+}
+
+void goLeft() {
+  Serial.println("LEFT");
+  digitalWrite(leftMotorForwardPin, LOW);
+  digitalWrite(leftMotorBackwardPin, HIGH);
+  digitalWrite(rightMotorForwardPin, HIGH);
+  digitalWrite(rightMotorBackwardPin, LOW);
 }
